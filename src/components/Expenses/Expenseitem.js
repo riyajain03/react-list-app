be2934a0e1d2a51d648aa0d000e534a90c6628b9
riyajain03 <jriya1099@gmail.com>
2023-07-15 19:23:12 +0530

    Initial commit - This project is simple UI for list app - Have learned about components, UI, props - JSX, root renderers

---
+++ src/components/Expenses/Expenseitem.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import Card from '../UI/Card';
+import './Expenseitem.css';
+import ExpenseDate from './ExpenseDate';
+
+
+function ExpenseItem(props) {
+
+    // const expenseDate = new Date (2021 , 2 ,28);
+
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 294.67;
+    
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
